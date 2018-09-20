b0a5869670ff0000a999c955d4613f8c80cfc8cf
hibinotatsuya <hibinotatsuya@gmail.com>
2018-09-20 17:49:34 +0900

    とりあえず完成

---
+++ public/form.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>ファイル送信フォーム</title>
+</head>
+<body>
+  <h1>ファイル送信フォーム</h1>
+  <form method="post" action="index.php" enctype="multipart/form-data">
+    hash(sha256(KEY+SECRET)) <input type="text" name="hash" placeholder="abcd1234"><br>
+    bucket <input type="text" name="bucket" placeholder="hogeimage"><br>
+    object_key <input type="text" name="object_key" placeholder="hoge/fuga.jpg"><br>
+    file <input type="file" name="file"><br>
+    <input type="submit" value="送信">
+  </form>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,2 +1,33 @@
 <?php
+require_once('../config/config.php');
 
+// パラメーターのチェック
+if (empty($_FILES) || empty($_POST) || empty($_POST['hash']) || empty($_POST['bucket']) || empty($_POST['object_key'])) {
+	exit;
+}
+
+// hashのチェック
+$hash = hash('sha256', KEY . SECRET);
+if ($hash != $_POST['hash']) {
+	echo 'hash_error';
+	exit;
+}
+
+// ファイルコピー
+$path = './' . $_POST['bucket'] . '/' . $_POST['object_key'];
+if (strpos($path, '..') !== false) {
+	echo 'path_error';
+	exit;
+}
+
+$dir = dirname($path);
+if (!file_exists($dir)) {
+	mkdir($dir, 0777, true);
+}
+
+$bool = copy($_FILES['file']['tmp_name'], $path);
+if ($bool) {
+    echo 'ok';
+} else {
+	echo 'file_error';
+}
